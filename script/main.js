d7a9c9400555340ab1a5ba5725fce5295165ee28
ana diz <ana_diz137@hotmail.es>
2021-01-28 11:21:46 +0100

    add evaluacion intermedia

--- script/main.js
+++ script/main.js
@@ -1,3 +1,26 @@
-'use strict';
+"use strict";
+const numberElement = document.querySelector(".js-number");
+const buttomElement = document.querySelector(".js-button");
+const tipElement = document.querySelector(".js-tip");
+const randomNumber = getRandomNumber(100);
+console.log(randomNumber);
 
-document.querySelector('h1').innerHTML = '¡Hola mundo!';
+function handleclick(event) {
+  /* escribir numero y darle a prueba*/
+  const number = parseInt(numberElement.value);
+
+  if (number > randomNumber) {
+    tipElement.value = "demasiado alto";
+  } else if (number < randomNumber) {
+    tipElement.value = "demasiado bajo";
+  } else if (number === randomNumber) {
+    tipElement.value = "has ganado campeona";
+  }
+}
+/*Cuando la jugadora introduzca un número mayor que el aleatorio y pulse en Prueba:
+Demasiado alto.*/
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
+}
+
+buttomElement.addEventListener("click", handleclick);
